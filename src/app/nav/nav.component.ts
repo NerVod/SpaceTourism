3dc4002b9498ae535be31955e031a606a1a5fcb5
NerVod <benjamin.jeannerot@gmail.com>
2022-07-21 18:01:59 +0200

    nav component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  public getScreenWidth: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth
+  }
+
+  @HostListener("window:resize", ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth
+  }
+
+}
