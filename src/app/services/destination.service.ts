dd873db8a72829e31a66e711e4323fc7cecb1022
NerVod <benjamin.jeannerot@gmail.com>
2022-07-28 19:47:24 +0200

    destination service

--- src/app/services/destination.service.ts
+++ src/app/services/destination.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DestinationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPlanet(planet:string):Observable<any> {
+    console.log(`adresse envoi requête : ${environment.baseURL}/destination/${planet}`)
+    return this.http.get(`${environment.baseURL}/destination/${planet}`)
+  }
+
+
+
+
+
+
+
 }
