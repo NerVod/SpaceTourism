5f35771479bc0460c360828f7cfd2e9900d4acfa
NerVod <benjamin.jeannerot@gmail.com>
2022-08-02 11:53:20 +0200

    circle home & link +  nav destination hover / select

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -13,6 +13,10 @@ export class DestinationComponent implements OnInit {
   survolMars!: boolean;
   survolEuropa!: boolean;
   survolTitan!: boolean;
+  selectMoon! : boolean
+  selectMars! : boolean
+  selectEuropa! : boolean
+  selectTitan! : boolean
 
   @Input() dataPlanet$: any
 
@@ -23,22 +27,37 @@ export class DestinationComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const destinationId = this.route.snapshot.params['id'];
+    // const destinationId = this.route.snapshot.params['id'];
   // this.destinationService.getPlanet('moon').subscribe((data: object) => this.dataPlanet$ = data)
-  this.getMoon()
+  this.getMoon();
+  this.selectMoon = true
   }
 
 getMoon(): void {
+  this.razSelected();
+  this.selectMoon = true
   this.destinationService.getPlanet('moon').subscribe((data: object) => this.dataPlanet$ = data)
 }
 getMars(): void {
+  this.razSelected();
+  this.selectMars = true;
   this.destinationService.getPlanet('mars').subscribe((data: object) => this.dataPlanet$ = data)
 }
 getEuropa(): void {
+  this.razSelected();
+  this.selectEuropa = true;
   this.destinationService.getPlanet('europa').subscribe((data: object) => this.dataPlanet$ = data)
 }
 getTitan(): void {
+  this.razSelected();
+  this.selectTitan = true;
   this.destinationService.getPlanet('titan').subscribe((data: object) => this.dataPlanet$ = data)
+}
+razSelected(){
+  this.selectMoon = false;
+  this.selectMars = false;
+  this.selectEuropa = false;
+  this.selectTitan = false
 }
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  circleHover!: boolean
+
   constructor() { }
 
   ngOnInit(): void {
+    this.circleHover = false
   }
 
 }
